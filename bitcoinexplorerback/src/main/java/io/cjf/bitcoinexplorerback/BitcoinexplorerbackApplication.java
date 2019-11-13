bb0566caadd4c26d4deb9485865489ea34d723fd
chenjiefei <nlpx_dc@hotmail.com>
2019-11-13 10:50:13 +0800

    first init backend

---
+++ bitcoinexplorerback/src/main/java/io/cjf/bitcoinexplorerback/BitcoinexplorerbackApplication.java
@@ -0,0 +1,13 @@
+package io.cjf.bitcoinexplorerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinexplorerbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoinexplorerbackApplication.class, args);
+    }
+
+}
---
+++ bitcoinexplorerback/src/test/java/io/cjf/bitcoinexplorerback/BitcoinexplorerbackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cjf.bitcoinexplorerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinexplorerbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
