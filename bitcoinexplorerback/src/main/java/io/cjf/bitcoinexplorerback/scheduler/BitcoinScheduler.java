2e7cbc768c55a2a5d669fd59c3b6f1703755d7d8
chenjiefei <nlpx_dc@hotmail.com>
2019-11-20 17:00:31 +0800

    draft tx unconfirmed

--- bitcoinexplorerback/src/main/java/io/cjf/bitcoinexplorerback/scheduler/BitcoinScheduler.java
+++ bitcoinexplorerback/src/main/java/io/cjf/bitcoinexplorerback/scheduler/BitcoinScheduler.java
@@ -5,12 +5,15 @@ import io.cjf.bitcoinexplorerback.client.BitcoinRest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 public class BitcoinScheduler {
@@ -22,7 +25,10 @@ public class BitcoinScheduler {
 
     private JSONObject originMempoolTx = new JSONObject();
 
-    private List<JSONObject> deltaTx = new LinkedList<>();
+    private List<JSONObject> deltaTxes = new LinkedList<>();
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
 
 //    @Scheduled(cron = "${bitcoin.sync.interval}")
 //    public void syncData(){
@@ -35,20 +41,38 @@ public class BitcoinScheduler {
 
         JSONObject newMempoolTx = bitcoinRest.getMempoolContents();
 
+        int originSize = originMempoolTx.size();
+        int newSize = newMempoolTx.size();
+        if (newSize <= originSize){
+            return;
+        }
+
         for (Map.Entry<String, Object> entry : newMempoolTx.entrySet()) {
             String key = entry.getKey();
             if (!originMempoolTx.containsKey(key)){
                 JSONObject addJson = newMempoolTx.getJSONObject(key);
                 addJson.put("txid", key);
-                deltaTx.add(addJson);
+                deltaTxes.add(addJson);
             }
         }
 
         //todo push delta tx
-        logger.info("delta tx: {}", deltaTx);
-        logger.info("delta size: {}", deltaTx.size());
+        logger.info("delta tx: {}", deltaTxes);
+        logger.info("delta size: {}", deltaTxes.size());
 
-        deltaTx = new LinkedList<>();
+        List<JSONObject> deltaTxesJsons = deltaTxes.stream().map(t -> {
+            JSONObject tJson = new JSONObject();
+            tJson.put("txid", t.getString("txid"));
+            tJson.put("wtxid", t.getString("wtxid"));
+            tJson.put("time", t.getLong("time"));
+            //todo calculate amount
+
+            return tJson;
+        }).collect(Collectors.toList());
+        List<JSONObject> sortedDeltaTxesJsons = deltaTxesJsons.stream().sorted(Comparator.comparingLong(t -> t.getLong("time"))).collect(Collectors.toList());
+        simpMessagingTemplate.convertAndSend("/bitcoin/deltaTx", sortedDeltaTxesJsons);
+
+        deltaTxes = new LinkedList<>();
         originMempoolTx = newMempoolTx;
 
         logger.info("end sync mempool tx");
---
+++ bitcoinexplorerfront/viewmodels/TxUnconfirmedIndex.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        transactions: []
+    },
+    mounted() {
+        console.log("view mounted");
+        // this.bitcoinPushConnect();
+    },
+    methods: {
+        bitcoinPushConnect() {
+            console.log('bitcoin push connecting');
+
+            this.socket = new SockJS('http://localhost:8080/bitcoinexplorerpush');
+            this.stompclient = Stomp.over(this.socket);
+            this.stompclient.connect({}, frame => {
+                console.log(frame);
+                this.stompclient.subscribe('/bitcoin/deltaTx', function (data) {
+                    console.log(data)
+                });
+            });
+
+        },
+        handleConnect() {
+            console.log('connect click');
+            this.bitcoinPushConnect();
+        }
+    }
+})
